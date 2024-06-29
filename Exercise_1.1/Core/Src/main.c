30db3967921d189be45e8b922235b2f612e050a1
Agustin Vazquez <vazquezagostino@gmail.com>
2024-06-29 22:18:47 +0100

    Modificación de archivo main.c

--- Exercise_1.1/Core/Src/main.c
+++ Exercise_1.1/Core/Src/main.c
@@ -1,177 +1,108 @@
-/* USER CODE BEGIN Header */
-/**
-  ******************************************************************************
-  * @file           : main.c
-  * @brief          : Main program body
-  ******************************************************************************
-  * @attention
-  *
-  * Copyright (c) 2024 STMicroelectronics.
-  * All rights reserved.
-  *
-  * This software is licensed under terms that can be found in the LICENSE file
-  * in the root directory of this software component.
-  * If no LICENSE file comes with this software, it is provided AS-IS.
-  *
-  ******************************************************************************
-  */
-/* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
-#include "main.h"
+#include "main.h" // Incluye el archivo de cabecera principal del proyecto
 
-/* Private includes ----------------------------------------------------------*/
-/* USER CODE BEGIN Includes */
+/* Function Prototypes -------------------------------------------------------*/
+void SystemClock_Config(void);  // Configuración del reloj del sistema
+static void MX_GPIO_Init(void); // Inicialización de los pines GPIO
 
-/* USER CODE END Includes */
-
-/* Private typedef -----------------------------------------------------------*/
-/* USER CODE BEGIN PTD */
-
-/* USER CODE END PTD */
-
-/* Private define ------------------------------------------------------------*/
-/* USER CODE BEGIN PD */
-
-/* USER CODE END PD */
-
-/* Private macro -------------------------------------------------------------*/
-/* USER CODE BEGIN PM */
-
-/* USER CODE END PM */
-
-/* Private variables ---------------------------------------------------------*/
-
-/* USER CODE BEGIN PV */
-
-/* USER CODE END PV */
-
-/* Private function prototypes -----------------------------------------------*/
-void SystemClock_Config(void);
-/* USER CODE BEGIN PFP */
-
-/* USER CODE END PFP */
-
-/* Private user code ---------------------------------------------------------*/
-/* USER CODE BEGIN 0 */
-
-/* USER CODE END 0 */
-
-/**
-  * @brief  The application entry point.
-  * @retval int
-  */
+/* Main Program --------------------------------------------------------------*/
 int main(void)
 {
 
-  /* USER CODE BEGIN 1 */
-
-  /* USER CODE END 1 */
-
-  /* MCU Configuration--------------------------------------------------------*/
-
-  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
-  HAL_Init();
-
-  /* USER CODE BEGIN Init */
-
-  /* USER CODE END Init */
-
-  /* Configure the system clock */
-  SystemClock_Config();
-
-  /* USER CODE BEGIN SysInit */
-
-  /* USER CODE END SysInit */
-
-  /* Initialize all configured peripherals */
-  /* USER CODE BEGIN 2 */
-
-  /* USER CODE END 2 */
+  HAL_Init(); // Inicializa la biblioteca HAL (Hardware Abstraction Layer)
+  SystemClock_Config(); // Configura el reloj del sistema
+  MX_GPIO_Init(); // Inicializa los pines GPIO
 
   /* Infinite loop */
-  /* USER CODE BEGIN WHILE */
   while (1)
   {
-    /* USER CODE END WHILE */
+    // Enciende LED durante 200ms
+    HAL_GPIO_WritePin(GPIOB, GPIO_PIN_0, GPIO_PIN_SET); // PIN_SET: Enciende LED1 (PB0)
+    HAL_Delay(200); // Esperar 200 ms
+    //Apaga todos los LEDs durante 200ms
+    HAL_GPIO_WritePin(GPIOB, GPIO_PIN_0, GPIO_PIN_RESET); // PIN_RESET: Apaga LED1 (PB0)
+    HAL_Delay(200); // Esperar 200 ms
 
-    /* USER CODE BEGIN 3 */
+    HAL_GPIO_WritePin(GPIOB, GPIO_PIN_7, GPIO_PIN_SET); // PIN_SET: Enciende LED2 (PB7)
+    HAL_Delay(200);
+    HAL_GPIO_WritePin(GPIOB, GPIO_PIN_7, GPIO_PIN_RESET); // PIN_RESET: Apaga LED2 (PB7)
+    HAL_Delay(200);
+
+    HAL_GPIO_WritePin(GPIOB, GPIO_PIN_14, GPIO_PIN_SET); // PIN_SET: Enciende LED3 (PB14)
+    HAL_Delay(200);
+    HAL_GPIO_WritePin(GPIOB, GPIO_PIN_14, GPIO_PIN_RESET); // PIN_RESET: Apaga LED3 (PB14)
+    HAL_Delay(200);
   }
-  /* USER CODE END 3 */
 }
 
-/**
-  * @brief System Clock Configuration
-  * @retval None
-  */
+/* Configuración del Reloj del Sistema ---------------------------------------*/
 void SystemClock_Config(void)
 {
-  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
-  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0}; // Estructura para la configuración de los osciladores
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0}; // Estructura para la configuración de los relojes
 
-  /** Configure the main internal regulator output voltage
-  */
-  __HAL_RCC_PWR_CLK_ENABLE();
-  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE3);
+  __HAL_RCC_PWR_CLK_ENABLE(); // Habilita el reloj para el controlador de energía
+  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE3); // Configura el regulador de voltaje
 
-  /** Initializes the RCC Oscillators according to the specified parameters
-  * in the RCC_OscInitTypeDef structure.
-  */
-  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
-  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
-  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
-  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI; // Usa el oscilador interno de alta velocidad (HSI)
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON; // Enciende el HSI
+  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT; // Usa el valor de calibración predeterminado
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE; // No usa el PLL
   if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
   {
-    Error_Handler();
+    Error_Handler(); // Manejo de errores en la configuración del oscilador
   }
 
-  /** Initializes the CPU, AHB and APB buses clocks
-  */
-  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
-                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
-  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI;
-  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
-  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK |
+                                RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2; // Configura los tipos de reloj
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI; // Usa HSI como fuente de reloj del sistema
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1; // Configura el divisor del reloj AHB
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1; // Configura el divisor del reloj APB1
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1; // Configura el divisor del reloj APB2
 
   if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK)
   {
-    Error_Handler();
+    Error_Handler(); // Manejo de errores en la configuración del reloj
   }
 }
 
-/* USER CODE BEGIN 4 */
+/* Inicialización de los Pines GPIO ------------------------------------------*/
+static void MX_GPIO_Init(void)
+{
+  GPIO_InitTypeDef GPIO_InitStruct = {0}; // Estructura para la configuración de GPIO
 
-/* USER CODE END 4 */
+  __HAL_RCC_GPIOB_CLK_ENABLE(); // Habilita el reloj para el puerto GPIOB
 
-/**
-  * @brief  This function is executed in case of error occurrence.
-  * @retval None
-  */
+  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_0 | GPIO_PIN_7 | GPIO_PIN_14, GPIO_PIN_RESET); // Configura el nivel de los pines (bajo)
+
+  GPIO_InitStruct.Pin = GPIO_PIN_0 | GPIO_PIN_7 | GPIO_PIN_14; // Configura los pines PB0, PB7 y PB14
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP; // Configura los pines como salida push-pull
+  /*
+   *
+   *
+   * Un pin en "push-pull" tiene 2 estados:
+   *
+   *
+   * Estado Alto (GPIO_PIN_SET): El pin se establece en un nivel de voltaje alto (generalmente igual
+   * al voltaje de alimentación del microcontrolador).
+   *
+   *
+   * Estado Bajo (GPIO_PIN_RESET): El pin se establece en un nivel de voltaje bajo (generalmente
+   * cercano a 0 voltios).
+   *
+   *
+   * */
+  GPIO_InitStruct.Pull = GPIO_NOPULL; // Sin resistencias pull-up o pull-down
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW; // Velocidad baja
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct); // Inicializa los pines GPIOB con la configuración especificada
+}
+
+/* Manejo de Errores ---------------------------------------------------------*/
 void Error_Handler(void)
 {
-  /* USER CODE BEGIN Error_Handler_Debug */
-  /* User can add his own implementation to report the HAL error return state */
-  __disable_irq();
+  __disable_irq(); // Desactiva todas las interrupciones
   while (1)
   {
   }
-  /* USER CODE END Error_Handler_Debug */
 }
 
-#ifdef  USE_FULL_ASSERT
-/**
-  * @brief  Reports the name of the source file and the source line number
-  *         where the assert_param error has occurred.
-  * @param  file: pointer to the source file name
-  * @param  line: assert_param error line source number
-  * @retval None
-  */
-void assert_failed(uint8_t *file, uint32_t line)
-{
-  /* USER CODE BEGIN 6 */
-  /* User can add his own implementation to report the file name and line number,
-     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
-  /* USER CODE END 6 */
-}
-#endif /* USE_FULL_ASSERT */
