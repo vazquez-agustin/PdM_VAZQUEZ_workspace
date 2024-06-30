3ade0f3e8af3b8bcbb7fd4fd0d840f6e7e85ba0c
Agustin Vazquez <vazquezagostino@gmail.com>
2024-07-01 00:25:42 +0100

    Modificación de archivo main.c

--- Exercise2/Core/Src/main.c
+++ Exercise2/Core/Src/main.c
@@ -2,92 +2,76 @@
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
 #include "gpio.h"
+#include "stm32f4xx_hal.h"
 
-/* Private includes ----------------------------------------------------------*/
-/* USER CODE BEGIN Includes */
+// Implementación de las funciones de retardo no bloqueante
+void delayInit(delay_t *delay, tick_t duration) {
+    delay->duration = duration;
+    delay->running = false;
+}
 
-/* USER CODE END Includes */
+bool_t delayRead(delay_t *delay) {
+    if (!delay->running) {
+        delay->startTime = HAL_GetTick();
+        delay->running = true;
+        return false;
+    } else {
+        tick_t currentTime = HAL_GetTick();
+        if (currentTime - delay->startTime >= delay->duration) {
+            delay->running = false;
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
 
-/* Private typedef -----------------------------------------------------------*/
-/* USER CODE BEGIN PTD */
+void delayWrite(delay_t *delay, tick_t duration) {
+    delay->duration = duration;
+}
 
-/* USER CODE END PTD */
 
 /* Private define ------------------------------------------------------------*/
-/* USER CODE BEGIN PD */
-
-/* USER CODE END PD */
-
-/* Private macro -------------------------------------------------------------*/
-/* USER CODE BEGIN PM */
-
-/* USER CODE END PM */
-
-/* Private variables ---------------------------------------------------------*/
-
-/* USER CODE BEGIN PV */
-
-/* USER CODE END PV */
+#define LED_PIN GPIO_PIN_0
+#define LED_GPIO_PORT GPIOB
 
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
-/* USER CODE BEGIN PFP */
 
-/* USER CODE END PFP */
 
-/* Private user code ---------------------------------------------------------*/
-/* USER CODE BEGIN 0 */
-
-/* USER CODE END 0 */
-
-/**
-  * @brief  The application entry point.
-  * @retval int
-  */
 int main(void)
 {
 
-  /* USER CODE BEGIN 1 */
-
-  /* USER CODE END 1 */
-
   /* MCU Configuration--------------------------------------------------------*/
-
-  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
   HAL_Init();
 
-  /* USER CODE BEGIN Init */
-
-  /* USER CODE END Init */
-
-  /* Configure the system clock */
   SystemClock_Config();
 
-  /* USER CODE BEGIN SysInit */
-
-  /* USER CODE END SysInit */
-
-  /* Initialize all configured peripherals */
   MX_GPIO_Init();
-  /* USER CODE BEGIN 2 */
 
-  /* USER CODE END 2 */
+
+  delay_t ledDelay;
+  delayInit(&ledDelay, 100);
+
+  // Estado del LED
+  bool_t ledState = false;
 
   /* Infinite loop */
-  /* USER CODE BEGIN WHILE */
   while (1)
   {
-    /* USER CODE END WHILE */
+	  // Verifica si el retardo ha terminado
+	  if (delayRead(&ledDelay)) {
+		  // Cambia el estado del LED
+	      ledState = !ledState;
+	      HAL_GPIO_WritePin(LED_GPIO_PORT, LED_PIN, ledState ? GPIO_PIN_SET : GPIO_PIN_RESET);
 
-    /* USER CODE BEGIN 3 */
+	      // Reinicia el retardo
+	      delayInit(&ledDelay, 100);
+      }
   }
   /* USER CODE END 3 */
 }
 
-/**
-  * @brief System Clock Configuration
-  * @retval None
-  */
 void SystemClock_Config(void)
 {
   RCC_OscInitTypeDef RCC_OscInitStruct = {0};
@@ -125,14 +109,8 @@ void SystemClock_Config(void)
   }
 }
 
-/* USER CODE BEGIN 4 */
 
-/* USER CODE END 4 */
 
-/**
-  * @brief  This function is executed in case of error occurrence.
-  * @retval None
-  */
 void Error_Handler(void)
 {
   /* USER CODE BEGIN Error_Handler_Debug */
@@ -143,20 +121,3 @@ void Error_Handler(void)
   }
   /* USER CODE END Error_Handler_Debug */
 }
-
-#ifdef  USE_FULL_ASSERT
-/**
-  * @brief  Reports the name of the source file and the source line number
-  *         where the assert_param error has occurred.
-  * @param  file: pointer to the source file name
-  * @param  line: assert_param error line source number
-  * @retval None
-  */
-void assert_failed(uint8_t *file, uint32_t line)
-{
-  /* USER CODE BEGIN 6 */
-  /* User can add his own implementation to report the file name and line number,
-     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
-  /* USER CODE END 6 */
-}
-#endif /* USE_FULL_ASSERT */
